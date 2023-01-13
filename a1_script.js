7190f8bc79b60371206c554cb7a07046a9328257
DESKTOP-M4EK23H\isaak <isaakddyck@hotmail.com>
2023-01-13 16:35:38 -0600

    Added a candies const -Calculates and adds together the candy_count number from pokemon -Skips over it if it is undefined and does not have the candy_count attached to it -Gets the average from the total number of Pokemon -Prints out what the average is and the total amount of Pokemon
    Also renamed name_and_type to info to be better fitting because it displays much more than just that.

--- a1_script.js
+++ a1_script.js
@@ -2,17 +2,27 @@ import data from './pokedex.json' assert { type: 'json' };
 
 const total = document.getElementById("total");
 const output = document.getElementById("output");
+const candies = document.getElementById("candies");
 const average_spawns = document.getElementById("average_spawns");
 
-let name_and_type = "";
+//Declare varaibles that get used in the for loops
+let info = "";
 let spawn_avg = 0;
+let candy_count = 0;
 
 for (let i = 0; i < data.pokemon.length; i++) {
-    name_and_type += `<div id="pokemon"><p><b>Name:</b> ${data.pokemon[i].name}</p>
+    info += `<div id="pokemon"><p><b>Name:</b> ${data.pokemon[i].name}</p>
                       <p><b>Number:</b> ${data.pokemon[i].num}</p>
                       <p><b>Type:</b> ${data.pokemon[i].type}</p>
                       <p><img src="${data.pokemon[i].img}"></p>
                       </div>`;
+}
+
+for (let i = 0; i < data.pokemon.length; i++) {
+  if (data.pokemon[i].candy_count != undefined) {
+    let candy_calc = data.pokemon[i].candy_count;
+    candy_count += candy_calc;
+  }
 }
 
 for (let i = 0; i < data.pokemon.length; i++) {
@@ -22,15 +32,17 @@ for (let i = 0; i < data.pokemon.length; i++) {
 
 //Shorten to 2 decimal places as it was outputting a large number with over 10 deciaml places.
 let spawn_dec = (spawn_avg/data.pokemon.length).toFixed(2);
+let candy_avg = (candy_count/data.pokemon.length).toFixed(2);
 
 //Lists the amount of Pokemon in the json file.
 //Also displays what the first[0] and last [.length-1] Pokemon are. It will always
 //display the last one in the list so it can be updated and still work.
 total.innerHTML = (`<p>The total amount of Pokemon included in this list is ${data.pokemon.length}.</p>
-<p>This includes all of the Generation 1 Pokemon ranging from ${data.pokemon[0].name} to ${data.pokemon[data.pokemon.length-1].name}.</p></div>`);
+<p>This includes all of the Generation 1 Pokemon ranging from ${data.pokemon[0].name} to ${data.pokemon[data.pokemon.length-1].name}.</p>`);
 
-//List the name and type of the Pokemon.
-output.innerHTML = name_and_type;
+//List the info of the Pokemon.
+output.innerHTML = info;
 
 //Display the amount of pokemon and what the average is across all of them.
 average_spawns.innerHTML = (`The average for the 'avg_spawns' value across all ${data.pokemon.length} Pokemon is ${spawn_dec}.`);
+candies.innerHTML = (`The average for the 'candy_count' value across all ${data.pokemon.length} Pokemon is ${candy_avg}.`);
