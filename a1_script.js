540dbc62515cda05817a056bf5969b28ca9320db
DESKTOP-M4EK23H\isaak <isaakddyck@hotmail.com>
2023-01-16 14:37:56 -0600

    Added a new for loop that adds together every number from the spawn_chance in the json file. It then averages the total and sets it to 2 decimal places, and then prints it out to the chance div. chance div was also added to the HTML file, along with some spacing to make everything look less clustered.

--- a1_script.js
+++ a1_script.js
@@ -6,11 +6,13 @@ const total = document.getElementById("total");
 const output = document.getElementById("output");
 const candies = document.getElementById("candies");
 const average_spawns = document.getElementById("average_spawns");
+const chance = document.getElementById("chance");
 
 //Declare varaibles that get used in the for loops
 let info = "";
-let spawn_avg = 0;
-let candy_count = 0;
+let spawn_avg = 0; //Spawn average
+let candy_count = 0; //Candy count
+let spawn_chn = 0; //Spawn chance
 
 //For loop that runs for however many pokemon are in the json file.
 for (let i = 0; i < data.pokemon.length; i++) {
@@ -38,14 +40,22 @@ for (let i = 0; i < data.pokemon.length; i++) {
 
 //For loop that runs for however many pokemon are in the json file
 for (let i = 0; i < data.pokemon.length; i++) {
-  //Add to the spawn_avg_calc let from the avg_spawns value from the json file.
+  //Add to the spawn_avg let from the avg_spawns value from the json file.
   let spawn_avg_calc = data.pokemon[i].avg_spawns;
   spawn_avg += spawn_avg_calc;
+}
+
+//For loop that runs for however many pokemon are in the json file
+for (let i = 0; i < data.pokemon.length; i++) {
+  //Add to the spawn_chn let from the avg_spawns value from the json file.
+  let spawn_chn_calc = data.pokemon[i].spawn_chance;
+  spawn_chn += spawn_chn_calc;
 }
 
 //Shorten to 2 decimal places as it was outputting a large number with over 10 deciaml places.
 let spawn_dec = (spawn_avg/data.pokemon.length).toFixed(2);
 let candy_avg = (candy_count/data.pokemon.length).toFixed(2);
+let chance_avg = (spawn_chn/data.pokemon.length).toFixed(2);
 
 //Lists the amount of Pokemon in the json file.
 //Also displays what the first[0] and last [.length-1] Pokemon are. It will always
@@ -60,3 +70,4 @@ output.innerHTML = info;
 //Display the amount of pokemon and what the average is across all of them.
 average_spawns.innerHTML = (`The average for the 'avg_spawns' value across all <b>${data.pokemon.length}</b> Pokemon is <b>${spawn_dec}</b>.`);
 candies.innerHTML = (`The average for the 'candy_count' value across all <b>${data.pokemon.length}</b> Pokemon is <b>${candy_avg}</b>.`);
+chance.innerHTML = (`The average for the 'spawn_average' value across all <b>${data.pokemon.length}</b> Pokemon is <b>${chance_avg}</b>.`);
